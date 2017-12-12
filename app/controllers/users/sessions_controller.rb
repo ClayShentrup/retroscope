f5eb5e57b5855a30f832d1007f62cff9f1241958
Clay Shentrup <clay@periscopedata.com>
2017-12-12 00:45:33 -0800

    Fix broken authentication
    In a previous story, we serialized the user to get the creator
    email info for whiteboard items and action items. However, this
    caused Devise SessionsController to return User info in JSONAPI
    which broken ember-simple-auth.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,4 +6,12 @@ class Users::SessionsController < Devise::SessionsController
       user.update!(authentication_token: Devise.friendly_token)
     end
   end
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    resource.update!(authentication_token: Devise.friendly_token)
+    render(json: resource.slice(:authentication_token, :email))
+  end
 end
